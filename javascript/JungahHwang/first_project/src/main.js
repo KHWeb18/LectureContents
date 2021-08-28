e82ef2f13cd7c9c6ec5e2892c3a7f4932d2bb510
jaqkqh15 <jaqkqh15@naver.com>
2021-08-28 20:22:34 +0900

    [황정아] mini-project: vue-cookies

--- javascript/JungahHwang/first_project/src/main.js
+++ javascript/JungahHwang/first_project/src/main.js
@@ -3,19 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-import VueCookies from 'vue-cookies'
+import cooikes from 'vue-cookies'
 
 
 import 'material-design-icons/iconfont/material-icons.css'
 
 Vue.config.productionTip = false
 
-Vue.$cookies.config('7200')
+Vue.use(cooikes)
+Vue.$cookies.config('2h')
 
 new Vue({
   router,
   store,
   vuetify,
-  VueCookies,
   render: h => h(App)
 }).$mount('#app')
