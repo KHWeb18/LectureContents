47892c6e00671f275a111bc88add1f38955744fb
jaqkqh15 <jaqkqh15@naver.com>
2021-09-13 21:35:37 +0900

    [황정아] mini-project: 메인페이지

--- javascript/JungahHwang/first_project/src/views/home/Home.vue
+++ javascript/JungahHwang/first_project/src/views/home/Home.vue
@@ -1,24 +1,5 @@
 <template>
   <v-sheet class="home">
-    <!--
-    <h2>메인 페이지</h2>
-    <v-btn color="red" class="hidden-xs-only">xs(폰)</v-btn>
-    <v-btn color="orange" class="hidden-sm-only">sm(태블릿)</v-btn>
-    <v-btn color="yellow" class="hidden-md-only">md(노트북)</v-btn>
-    <v-btn color="green" class="hidden-lg-only">lg(데스크탑)</v-btn>
-
-    <router-link :to="{ name: 'Test' }" 
-      class="nav-link" 
-      active-class="active">Test
-    </router-link>
-    <router-link :to="{ name: 'ListTest' }" 
-      class="nav-link" 
-      active-class="active">ListTest
-    </router-link>
-
-    <v-btn @click="cookies">쿠키</v-btn>
-    -->
-
     <weather></weather>
 
     <v-row class="mt-5">
@@ -56,10 +37,7 @@ export default {
   created () {
   },
   methods: {
-    cookies () {
-      let userCookies = this.$cookies.get('user')
-      console.log(userCookies)
-    },
+    
 
   }
 }
