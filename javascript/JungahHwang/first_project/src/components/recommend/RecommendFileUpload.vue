90f488d872c12765e55ee9dd548cf7fd72a9febc
jaqkqh15 <jaqkqh15@naver.com>
2021-09-13 21:36:23 +0900

    [황정아] mini-project: 파일업로드

--- javascript/JungahHwang/first_project/src/components/recommend/RecommendFileUpload.vue
+++ javascript/JungahHwang/first_project/src/components/recommend/RecommendFileUpload.vue
@@ -13,7 +13,6 @@
 
 
 <script>
-//import axios from 'axios'
 
 export default {
   data () {
@@ -25,43 +24,15 @@ export default {
   methods: {
     select () {
       console.log(this.files)
-      this.url = URL.createObjectURL(this.files[0])
 
+      try {
+        this.url = URL.createObjectURL(this.files[0])
+      }catch(e) {
+        this.url = null
+      }
+      
       this.$emit('selectFile', this.files)
     },
-    // fileUpload () {
-    //   let formData = new FormData()
-
-    //   for (let i = 0; i < this.files.length; i++) {
-    //     formData.append('fileList', this.files[i])
-    //   }
-
-    //   formData.append('boardNo', 12)
-    //   formData.append('id', 'test')
-
-    //   console.log(formData)
-
-      
-    //   axios.post(`http://localhost:7777/fileUpload/recommend`, formData, {
-    //       headers: {
-    //         'Content-Type': 'multipart/form-data'
-    //       }
-    //     }).then (res => {
-    //       this.response = res.data
-
-    //       alert('사진 업로드 완료!')
-    //     }).catch (res => {
-    //       this.response = res.message
-    //     })
-
-    // },
-    // fileDownload () {
-    //   try {
-    //     return require(`../../../../FirstProject/images/12_test.jpg`)
-    //   } catch (e) {
-    //     return require(`@/assets/icon/logo.png`)
-    //   }
-    // }
   }
 }
 </script>
